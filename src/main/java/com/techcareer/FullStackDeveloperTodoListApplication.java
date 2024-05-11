cf4ba81ab9eabeb5577cdc92f76a293d8950a295
Furkan YILDIRIM <46926199+Furkany99@users.noreply.github.com>
2024-05-11 12:28:23 +0300

    Initial commit

---
+++ src/main/java/com/techcareer/FullStackDeveloperTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.techcareer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackDeveloperTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackDeveloperTodoListApplication.class, args);
+	}
+
+}
